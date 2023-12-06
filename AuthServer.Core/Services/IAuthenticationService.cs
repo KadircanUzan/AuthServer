977e355d4cdad81caf53d488d45b5dbafea6c398
Kadircan UZAN <kadircanuzan@gmail.com>
2023-12-06 21:04:25 +0300

    Apiler ile direkt erişim için gerekli olan Service sınıfı olarak IAuthenticationService oluşturuyoruz. Clientların göreceği bir sınıf olarak DTOs dosyası içerisinde giriş ekranında LoginDto sınıfımız ile erişim sağlanan özellikleri belirliyoruz. Burada ClientSecret özelliği, OAuth 2.0 kimlik doğrulama akışında kullanılan bir güvenlik anahtarıdır.

---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenRefleshToken(string refleshToken);
+
+        Task<Response<NoDataDto>> RevokeRefleshToken(string refleshToken);
+
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+
+    }
+}
