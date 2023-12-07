2e855f8efb905bac5ffb03b5a974978a039c8cd9
Kadircan UZAN <kadircanuzan@gmail.com>
2023-12-07 11:30:57 +0300

    UserRefleshToken ve UserApp sınıflarının da Entity Framework Configuration ayarları da yapıldı ve AppDbContext içerisinde OnModelCreating metodu ile birlikte metot içerisinde builder.ApplyConfigurationsFromAssembly(GetType().Assembly); kullanılarak bütün Configuration sınıfları tek tek yazmak yerine proje içerisindeki IEntityTypeConfiguration<TEntity> bağlı tüm sınıfları proje içerisinden bul ve Assembly kaydet demiş olduk.

---
+++ AuthServer.Data/Configurations/UserRefleshTokenConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AuthServer.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthServer.Data.Configurations
+{
+    public class UserRefleshTokenConfiguration : IEntityTypeConfiguration<UserRefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<UserRefreshToken> builder)
+        {
+            builder.HasKey(id => id.UserId);
+            builder.Property(co=>co.Code).IsRequired().HasMaxLength(200);
+        }
+    }
+}
