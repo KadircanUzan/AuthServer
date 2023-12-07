0ea9d437d45d3279fe969854cfe01cbfb0222d59
Kadircan UZAN <kadircanuzan@gmail.com>
2023-12-07 03:37:32 +0300

    Nesnelerin veritabanındaki özelliklerini AuthServer.Data içerisinde Configurations klasörü altında IEntityTypeConfiguration interface den miras alarak oluşuruyoruz. Nesneleri 3 yol ile veritabanına ayarlayarak gönderebiliriz. 1.Yol Data Anatotion ile tercih edilmez.Çünkü model içerisinin temiz kalması proje için daha doğru olacaktır. 2.Yol Id verdiğimiz için migration dan sonra bu entity için özel olarak id ile tanımlanan özelliği primary key olarak tanımlamaktadır. 3.Yol IEntityTypeConfiguration ile sınıf implament edilerek özelliklerini belirtebiliriz. Product sınıfına ait olan Configuration işlemlerini gerçekleştirdik.

---
+++ AuthServer.Data/Configurations/ProductConfuguration.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthServer.Data.Configurations
+{
+    public class ProductConfuguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(id => id.Id);
+            builder.Property(na => na.Name).HasMaxLength(256).IsRequired();
+            builder.Property(st => st.Stock).IsRequired();
+            builder.Property(pr => pr.Price).IsRequired().HasColumnType("decimal(18,2)");
+            builder.Property(pr => pr.UserId).IsRequired();
+        }
+    }
+}
