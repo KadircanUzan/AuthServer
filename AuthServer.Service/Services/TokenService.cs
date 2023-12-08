36751761a8962c8f6e588c5a6f704c461174bd41
Kadircan UZAN <kadircanuzan@gmail.com>
2023-12-08 15:06:48 +0300

    TokenService içerisinde CreateRefreshToken metodu içerisinde 32 bitlik random bir değer üretecek metot oluşturuyoruz. RefleshToken set etmek için kullanacağız.

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Model;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+using System.Security.Cryptography;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+
+        private readonly UserManager<UserApp> _userManager;
+
+        private readonly CustomTokenOption _TokenOption;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> customTokenOption)
+        {
+            _userManager = userManager;
+            _TokenOption = customTokenOption.Value;
+        }
+
+        private string CreateRefleshToken()
+        {
+            //return Guid.NewGuid().ToString();
+            var numberByte = new Byte[32];
+            using var rnd=RandomNumberGenerator.Create();
+
+            rnd.GetBytes(numberByte);
+            return Convert.ToBase64String(numberByte);
+        }
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
