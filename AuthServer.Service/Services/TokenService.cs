416748c5e089a5daba9da7a41fa2ad0a670cbda5
Kadircan UZAN <kadircanuzan@gmail.com>
2023-12-08 15:27:42 +0300

    TokenService içerisinde Claim nesnesi döneceğimiz bir metot yazıyoruz.Metot ismi GetClaim() parametre olarak userApp ve audiences=>hangi tokenın hangi apilere istek yapıcağını belirlediğimiz parametre Kullanıcılar ile ilgili bilgileri istediğimiz userList diye bir değişken var buranın içerisinde de kullanıcının id sini Payload içerisinde görmek istiyorum.new Claim(ClaimTypes.NameIdentifier,userApp.Id), Yine kullanıcının email adresini almak isteyebiliriz.new Claim(JwtRegisteredClaimNames.Email,userApp.Email) Yine kullanının USERNAME bilgisini tutmak için  new Claim(ClaimTypes.Name,userApp.UserName), Her token için tokenlara ait Id de alabiliriz.  new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()) Seçilen api için bu tokenı oluşturabilmesi için  userList.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));

--- AuthServer.Service/Services/TokenService.cs
+++ AuthServer.Service/Services/TokenService.cs
@@ -5,6 +5,8 @@ using AuthServer.Core.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using SharedLibrary.Configurations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace AuthServer.Service.Services
@@ -26,10 +28,23 @@ namespace AuthServer.Service.Services
         {
             //return Guid.NewGuid().ToString();
             var numberByte = new Byte[32];
-            using var rnd=RandomNumberGenerator.Create();
+            using var rnd = RandomNumberGenerator.Create();
 
             rnd.GetBytes(numberByte);
             return Convert.ToBase64String(numberByte);
+        }
+
+        private IEnumerable<Claim> GetClaim(UserApp userApp, List<string> audiences)
+        {
+            var userList = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,userApp.Id),
+                new Claim(JwtRegisteredClaimNames.Email,userApp.Email),// new Claim("email",userApp.Email)de olabilir.
+                new Claim(ClaimTypes.Name,userApp.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+            userList.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+            return userList;
         }
         public TokenDto CreateToken(UserApp userApp)
         {
