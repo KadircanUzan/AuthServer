3653e7f6c7f14aed582ffef40452e46758e9dffd
Kadircan UZAN <kadircanuzan@gmail.com>
2023-12-11 21:30:04 +0300

    Program.cs içerisinde AddAuthentication metodu iki veya daha fazla tipte giriş yapılacağı zamam kullanılabilir.Örneğin biri kullanıclar için belirlenebilir diğeri firma bayilikleri için olabilir. builder.Services.AddAuthentication(options =>  {      options.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;  }); Bir token geldiğinde Doğrulama işlemlerini de belirleyebiliiyoruz. .AddJwtBearer("Bearer", options =>             {                 var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();                 options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()                 {                     ValidIssuer = tokenOptions.Issuer,                     ValidAudience = tokenOptions.Audience[0],                     IssuerSigningKey=SignService.GetSymmetricSecurityKey(tokenOptions.Issuer),
                        ValidateIssuerSigningKey=true,
                        ValidateAudience = true,
                        ValidateIssuer = true,
                        ValidateLifetime = true,
    
                        ClockSkew=TimeSpan.Zero
    
                    };

--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -6,6 +6,7 @@ using AuthServer.Core.UnitOfWork;
 using AuthServer.Data;
 using AuthServer.Data.Repositories;
 using AuthServer.Service.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SharedLibrary.Configurations;
@@ -20,11 +21,11 @@ namespace AuthServer.API
             //DI Register
             builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
             builder.Services.AddScoped<ITokenService, TokenService>();
-            builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepositories<>));
-            builder.Services.AddScoped(typeof(IServiceGeneric<,>),typeof(IServiceGeneric<,>));
-            builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositories<>));
+            builder.Services.AddScoped(typeof(IServiceGeneric<,>), typeof(IServiceGeneric<,>));
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            builder.Services.AddDbContext<AppDbContext>(options=>
+            builder.Services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), sqlOptions =>
                 {
@@ -40,15 +41,38 @@ namespace AuthServer.API
             }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
             // Add services to the container.
             builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+
             builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
 
             builder.Services.AddControllers();
-            
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            //JwtBearerDefaults.AuthenticationScheme yerine Bearer de yazabiliriz.
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 
+            }).AddJwtBearer("Bearer", options =>
+            {
+                var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience[0],
+                    IssuerSigningKey=SignService.GetSymmetricSecurityKey(tokenOptions.Issuer),
 
+                    ValidateIssuerSigningKey=true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+
+                    ClockSkew=TimeSpan.Zero
+
+                };
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
